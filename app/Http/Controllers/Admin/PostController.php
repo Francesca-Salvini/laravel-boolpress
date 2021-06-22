4dbb8961b60fd1cfb2c4ec179de687fba4c30f1d
Francesca Salvini <franci98sp@gmail.com>
2021-06-22 12:35:21 +0200

    edit con categories

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -109,9 +109,11 @@ class PostController extends Controller
     public function edit($id)
     {
         $post = Post::findOrFail($id);
+        $categories = Category::all();
 
         $data = [
-            'post' => $post
+            'post' => $post,
+            'categories' => $categories
         ];
 
         return view('admin.posts.edit', $data);
@@ -129,7 +131,8 @@ class PostController extends Controller
 
         $request->validate( [
             'title' => 'required|max:255',
-            'content' => 'required|max:65000'
+            'content' => 'required|max:65000',
+            'category_id' => 'nullable|exists:categories,id'
         ] );
 
         $modified_post_data = $request->all();
